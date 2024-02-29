ffa275fc146618508bd32c2d7263453c00745678
Nandu <nandagopal1066@gmail.com>
2024-02-29 14:47:24 +0530

    Added  local programs - initial commit

---
+++ src/java_programs/Pattern_2.java
@@ -0,0 +1,23 @@
+public class Pattern_2 {
+    public static void main(String[] args) {
+        int star = 6;
+        int space = -1;
+        for (int i = 1; i <= 5; i++) {
+            space++;
+            star--;
+            for (int j = 0; j < space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+// output:
+// *****
+// ****
+// ***
+// **
+// *
